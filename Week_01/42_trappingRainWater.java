fbd496845d476c36a204ad1e4a5b72336a9f49e2
zoumengfan <zoumengfan@hotmail.com>
2020-06-30 11:24:27 +0800

    update week01

--- Week_01/42_trappingRainWater.java
+++ Week_01/42_trappingRainWater.java
@@ -1,26 +1,29 @@
 class Solution {
     // solution 1
     public int trap(int[] height) {
-        int i = 0, j = height.length - 1;
-        while (i < j && height[i + 1] > height[i]) {
-            i++;
-        }
-        
-        while (j > i && height[j] < height[j - 1]) {
-            j--;
-        }
-        
         int result = 0;
+        int i = 0, j = height.length - 1;
+        
+        while (i < j && height[i] < height[i + 1]) {
+            ++i;
+        }
+        
+        while (i < j && height[j] < height[j - 1]) {
+            --j;
+        }
+        
         while (i < j) {
             int left = height[i];
             int right = height[j];
+            
             if (left < right) {
                 while (i < j && left > height[++i]) {
-                    result += (left - height[i]);
+                    result += left - height[i];
                 }
+                
             } else {
-                while (j > i && right > height[--j]) {
-                    result += (right - height[j]);
+                while (i < j && right > height[--j]) {
+                    result += right - height[j];
                 }
             }
         }
